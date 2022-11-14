11b481463863f30d543e646580a3c338d77d9607
Nguyễn Bách Tùng <bachtung144007@gmail.com>
2022-11-14 13:12:20 +0700

    Update __manifest__.py

--- web_responsive/__manifest__.py
+++ web_responsive/__manifest__.py
@@ -14,7 +14,7 @@
     'live_test_url_vi_VN': "https://v15demo-vn.viindoo.com",
     "author": "LasLabs, Tecnativa, ITerra, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
-    "installable": False,
+    "installable": True,
     "depends": ["web", "mail"],
     "development_status": "Production/Stable",
     "maintainers": ["Yajo", "Tardo", "SplashS"],
